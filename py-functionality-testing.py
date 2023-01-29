89d9a2a6e495a972db677ab117f29e803ab0b007
Jared Wood <jared.wood135@gmail.com>
2023-01-29 17:32:14 -0600

    Added some basic python testing with CLI

--- py-functionality-testing.py
+++ py-functionality-testing.py
@@ -1,3 +1,38 @@
 import os
 
-os.chdir('~/Desktop')
+# Changes directory successfully
+(os.chdir("/private/etc/"))
+print(os.getcwd())
+
+# Return bool if file exists
+print(os.path.isfile("zshrc"))
+
+#Opens file
+#os.open("/private/etc/zshrc")
+
+#Finds home directory
+home = os.path.expanduser( '~' )
+print(home)
+os.chdir(home)
+print(os.getcwd())
+
+#test
+#f = open("/private/etc/zshrc")
+#print(f.read())
+
+# Find then create file test
+def find_hiddentest_file():
+    home = os.path.expanduser( '~' )
+    os.chdir(home)
+    os.chdir("Desktop")
+    print(os.getcwd())
+    print(os.path.isfile("test"))
+
+def yes():
+    if find_hiddentest_file is True:
+        print("It exists")
+    else:
+        print("It does not exist")
+    
+find_hiddentest_file()
+yes()
