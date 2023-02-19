1f792e559f37ceb064f5856cecb22c55840dd872
Jared Wood <jared.wood135@gmail.com>
2023-02-19 14:51:46 -0600

    'Prompt Line Function' created and tested successfully

--- py-functionality-testing.py
+++ py-functionality-testing.py
@@ -11,7 +11,9 @@
 -   FUNCTIONS
     -   Import For Functions
     -   Main Menu
-    -   Colors
+    -   Prompt Line Modification
+    -   Misc List
+    -   Colors List
     -   Clear Terminal
 -   END PRODUCT
 '''
@@ -71,6 +73,9 @@ gray
 # =======================================================================================================
 
 # ==========> IMPORT FOR FUNCTIONS <==========
+'''
+All imports needed for this script to work properly
+'''
 import os
 
 # ==========> MAIN MENU <==========
@@ -86,38 +91,59 @@ def menu():
 
     # vvv MAIN MENU LIST vvv
     clear()
+    error = "\033[31mInvalid Input...\033[0m\n"
     while True:
-        print(
+        # vvv IF FILE DOESN'T EXIST vvv
+        if os.path.exists('test') == False:
+            create_file = input("\033[33m'test'\033[0m file not found...\nWould you like to create \033[33m'test'\033[0m file? \033[33m(Y/N)\033[0m\n")
+            if create_file.lower() == 'y':
+                clear()
+                print("Creating \033[33m'file'\033[0m...\n")
+                with open('test', 'w') as f:
+                    f.writelines('# =====> PROMPT <=====\n# Default Prompt:\n# PS1="%n@%m %1~ %# "\n\n# Current Prompt:\nPS1="%n@%m %1~ %# "\n\n\n# =====> ALIASES <=====')
+                    f.flush()
+                    f.close()
+                print("\033[33m'file'\033[0m created...\n")
+            elif create_file.lower() == 'n':
+                clear()
+                print("Have a great day!")
+                break
+            else:
+                clear()
+                print(error)
+        # vvv IF FILE EXISTS vvv        
+        elif os.path.exists('test') == True:
+            print(
             '\033[33m==========> MAIN MENU <==========\033[0m\n\n',
             '\033[36m(1) Command Line Modification\033[0m\n',
             '\033[36m(2) Alias Creation\033[0m\n',
             '\033[36m(3) Exit Program\033[0m\n'
-            )
-        # vvv INPUTS vvv
-        start = input('What would you like to do?:\n')
-        if start == '1':
-            clear()
-            cli_mod()
-        elif start == '2':
-            clear()
-            print('ALIAS MOD')
-        elif start == '3':
-            clear()
-            print('Have a great day!')
-            break
-        else:
-            clear()
-            print("Invalid Input...\n")
+                )
+            # vvv INPUTS vvv
+            start = input('What would you like to do?:\n')
+            if start == '1':
+                clear()
+                cli_mod()
+            elif start == '2':
+                clear()
+                print('ALIAS MOD')
+            elif start == '3':
+                clear()
+                print('Have a great day!')
+                break
+            else:
+                clear()
+                print(error)
 menu()
 
-# ==========> MODIFICATION <==========
+# ==========> PROMPT LINE MODIFICATION <==========
 '''
 Modify the command prompt of the command line interface.
 '''
 def cli_mod():
     clear()
-    colors_list = ['black', 'blue', 'cyan', 'green', 'magenta', 'red', 'white', 'yellow']
-    misc_list = ['bold', 'highlight']
+    colors_list = ['black', 'blue', 'cyan', 'green', 'magenta', 'red', 'white', 'yellow', 'none']
+    misc_list = ['bold', 'highlight', 'both', 'none']
     error = "\033[31mInvalid Input...\033[0m\n"
     while True:
 
@@ -153,10 +179,15 @@ def cli_mod():
             clear()
             while True:
                 colors()
-                print('"\033[33mHOST@USER\033[0m %1~ %# "\n')
+                print('\n"\033[33mHOST@USER\033[0m %1~ %# "\n')
                 host_user_color = input("What \033[33mcolor\033[0m do you want for your \033[33mstarting prompt\033[0m?\n")
                 if host_user_color.lower() in colors_list:
-                    break
+                    if host_user_color.lower() == 'none':
+                        host_user_result = (host_user)
+                        break
+                    else:
+                        host_user_result = ("%F{" + host_user_color + "}" + host_user + "%f")
+                        break
                 else:
                     clear()
                     print(error)
@@ -165,9 +196,25 @@ def cli_mod():
             clear()
             while True:
                 misc()
-                print('"\033[33mHOST@USER\033[0m %1~ %# "\n')
+                print('\n"\033[33mHOST@USER\033[0m %1~ %# "\n')
                 host_user_misc = input("What \033[33mmodifier\033[0m would you like to apply to your \033[33mstarting prompt\033[0m?\n")
-                if host_user_misc in misc_list:
+                if host_user_misc.lower() in misc_list:
+                    if host_user_misc.lower() == 'bold':
+                        host_user_mod_begin = '%B'
+                        host_user_mod_end = '%b'
+                        break
+                    elif host_user_misc.lower() == 'highlight':
+                        host_user_mod_begin = '%S'
+                        host_user_mod_end = '%s'
+                        break
+                    elif host_user_misc.lower() == 'both':
+                        host_user_mod_begin = '%B%S'
+                        host_user_mod_end = '%s%b'
+                        break
+                    elif host_user_misc.lower() == 'none':
+                        host_user_mod_begin = ''
+                        host_user_mod_end = ''
+                        break
                     break
                 else:
                     clear()
@@ -176,11 +223,16 @@ def cli_mod():
             # vvv WORKING DIRECTORY COLOR vvv
             clear()
             while True:
-                print('"HOST@USER \033[33m%1~\033[0m %# "\n')
                 colors()
+                print('\n"HOST@USER \033[33m%1~\033[0m %# "\n')
                 cwd_color = input("What \033[33mcolor\033[0m do you want for your \033[33mcurrent working directory\033[0m?\n")
-                if cwd_color in colors_list:
-                    break
+                if cwd_color.lower() in colors_list:
+                    if cwd_color.lower() == 'none':
+                        cwd_result = '%1~'
+                        break
+                    else:
+                        cwd_result = ("%F{" + cwd_color + "}%1~" + "%f")
+                        break
                 else:
                     clear()
                     print(error)
@@ -189,9 +241,25 @@ def cli_mod():
             clear()
             while True:
                 misc()
-                print('"HOST@USER \033[33m%1~\033[0m %# "\n')
+                print('\n"HOST@USER \033[33m%1~\033[0m %# "\n')
                 cwd_misc = input("What \033[33mmodifier\033[0m would you like to apply to your \033[33mcurrent working directory\033[0m?\n")
-                if cwd_misc in misc_list:
+                if cwd_misc.lower() in misc_list:
+                    if cwd_misc.lower() == 'bold':
+                        cwd_mod_begin = '%B'
+                        cwd_mod_end = '%b'
+                        break
+                    elif cwd_misc.lower() == 'highlight':
+                        cwd_mod_begin = '%S'
+                        cwd_mod_end = '%s'
+                        break
+                    elif cwd_misc.lower() == 'both':
+                        cwd_mod_begin = '%B%S'
+                        cwd_mod_end = '%s%b'
+                        break
+                    elif cwd_misc.lower() == 'none':
+                        cwd_mod_begin = ''
+                        cwd_mod_end = ''
+                        break
                     break
                 else:
                     clear()
@@ -206,10 +274,15 @@ def cli_mod():
             clear()
             while True:
                 colors()
-                print('"HOST@USER %1~ \033[33m%#\033[0m "\n')
+                print('\n"HOST@USER %1~ \033[33m%#\033[0m "\n')
                 end_prompt_color = input("What \033[33mcolor\033[0m do you want for your \033[33mending prompt\033[0m?\n")
-                if end_prompt_color in colors_list:
-                    break
+                if end_prompt_color.lower() in colors_list:
+                    if end_prompt_color.lower() == 'none':
+                        end_prompt_result = (end_prompt)
+                        break
+                    else:
+                        end_prompt_result = ("%F{" + end_prompt_color + "}" + end_prompt + "%f")
+                        break
                 else:
                     clear()
                     print(error)
@@ -218,23 +291,40 @@ def cli_mod():
             clear()
             while True:
                 misc()
-                print('"HOST@USER %1~ \033[33m%#\033[0m "\n')
+                print('\n"HOST@USER %1~ \033[33m%#\033[0m "\n')
                 end_prompt_misc = input("What \033[33mmodifier\033[0m do you want for your \033[33mending prompt\033[0m?\n")
-                if end_prompt_misc in misc_list:
+                if end_prompt_misc.lower() in misc_list:
+                    if end_prompt_misc.lower() == 'bold':
+                        end_prompt_mod_begin = '%B'
+                        end_prompt_mod_end = '%b'
+                        break
+                    elif end_prompt_misc.lower() == 'highlight':
+                        end_prompt_mod_begin = '%S'
+                        end_prompt_mod_end = '%s'
+                        break
+                    elif end_prompt_misc.lower() == 'both':
+                        end_prompt_mod_begin = '%B%S'
+                        end_prompt_mod_end = '%s%b'
+                        break
+                    elif end_prompt_misc.lower() == 'none':
+                        end_prompt_mod_begin = ''
+                        end_prompt_mod_end = ''
+                        break
                     break
                 else:
                     clear()
                     print(error)
 
             # vvv CREATING NEW PS1 vvv
+            clear()
             with open('test', 'r') as f:
                 lines = f.readlines()
             for i in range(len(lines)):
                 if lines[i].startswith('PS1'):
-                    lines[i] = lines[i].replace(lines[i], (f"PS1=\"{host_user} %1~ {end_prompt} \""))
+                    lines[i] = lines[i].replace(lines[i], (f"PS1=\"{host_user_mod_begin}{host_user_result}{host_user_mod_end} {cwd_mod_begin}{cwd_result}{cwd_mod_end} {end_prompt_mod_begin}{end_prompt_result}{end_prompt_mod_end} \"\n"))
             with open('test', 'w') as f:
                 f.writelines(lines)
-                print(lines)
+                print("\033[32mCommand Line Prompt Line Changed Successfully\033[0m\n")
                 break
 cli_mod()
 
@@ -267,7 +357,7 @@ if len(segments) > 1:
     hostname = segments[1].split(segments[2])[0]
 print(f"User: {username}, Host: {hostname}")
 
-# ==========> MISC FUNCTION <==========
+# ==========> MISC LIST FUNCTION <==========
 '''
 Outputs a list of additional modifications for the command prompt
 '''
@@ -276,9 +366,10 @@ def misc():
     print(f"{'--------------------' : ^20}|{'--------------------' : ^20}")
     print(f"{'BOLD TEXT' : ^20}|{'bold' : ^20}")
     print(f"{'HIGHLIGHTED TEXT' : ^20}|{'highlight' : ^20}")
-misc()
+    print(f"{'BOLD & HGHLGT' : ^20}|{'both' : ^20}")
+    print(f"{'NONE' : ^20}|{'none' : ^20}")
 
-# ==========> COLORS FUNCTION <==========
+# ==========> COLORS LIST FUNCTION <==========
 '''
 Outputs a list of all the colors available for use
 '''
@@ -293,6 +384,7 @@ def colors():
     print(f"\033[31m{'RED' : ^15}\033[0m|{'red' : ^15}")
     print(f"\033[37m{'WHITE' : ^15}\033[0m|{'white' : ^15}")
     print(f"\033[33m{'YELLOW' : ^15}\033[0m|{'yellow' : ^15}")
+    print(f"{'NONE' : ^15}|{'none' : ^15}")
 
 # ==========> CLEAR TERMINAL <==========
 '''
@@ -307,7 +399,7 @@ def clear():
 # END PRODUCT START
 # =======================================================================================================
 
-
+menu()
 
 # =======================================================================================================
 # END PRODUCT END
